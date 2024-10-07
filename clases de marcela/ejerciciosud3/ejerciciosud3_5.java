138e766ea5b47297b9f43425b674798b7327d846
Parsa-P3 <131161509+Parsa-P3@users.noreply.github.com>
2024-10-07 08:44:08 +0200

    ejerciciosud3_5.java

--- clases de marcela/ejerciciosud3/ejerciciosud3_5.java
+++ clases de marcela/ejerciciosud3/ejerciciosud3_5.java
@@ -5,7 +5,7 @@ public class ejerciciosud3_5 {
 
     public static void main(String[] args) {
         try (Scanner degiskenler = new Scanner(System.in)) {
-            System.out.println("para resolver el problema y = ax.ax + bx + c ");
+            System.out.println("para resolver el problema y = a* (x * x) + bx + c ");
 
             System.out.print("Ingresa la a: ");
             int a = degiskenler.nextInt();
@@ -19,7 +19,7 @@ public class ejerciciosud3_5 {
             System.out.print("Ingresa la x: ");
             int x = degiskenler.nextInt();
 
-            int resultado = a*b*a*b+b*x+c ;
+            int resultado = a*(x*x)+b*x+c ;
             System.out.print("Y es = " + resultado);
 
         }
